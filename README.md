86eb8f42adf9e75a35352e44db1a92d73f63870d
puhung <twenet1212@gmail.com>
2024-06-24 22:12:21 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Project Title: Question Based Learning
+
+**Introduction**: 
+This project aims to help user learn any field of knowledge in the form of Questions.
---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def main():
+    user_input = input("Please enter something.")
+    print('Your entered: ' + user_input)
+
+if __name__ == '__main__':
+    main()
+    
